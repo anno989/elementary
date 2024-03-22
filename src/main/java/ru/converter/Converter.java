2a57bc859d2c2acdeed12f6f9dece676cb05aaa3
Aleksandr <a.v.agafonov@mail.ru>
2024-03-23 02:28:49 +0300

    1.9 Конвертер валюты

--- src/main/java/ru/converter/Converter.java
+++ src/main/java/ru/converter/Converter.java
@@ -13,7 +13,7 @@ public class Converter {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
         float dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " euro.");
+        System.out.println("140 rubles are " + dollar + " dollar.");
     }
 }
 
