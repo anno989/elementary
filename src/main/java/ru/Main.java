f6186d00699ecb0fd15d55ad222e129b033f8e5d
Aleksandr <a.v.agafonov@mail.ru>
2024-03-22 13:29:36 +0300

    1.3. Арифметические операции

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -2,6 +2,15 @@ package ru;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int two = 2;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
     }
 }
