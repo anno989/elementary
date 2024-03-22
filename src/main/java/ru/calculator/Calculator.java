33c35163ecfd8b6bfe27c4addee268457a4ee89a
Aleksandr <a.v.agafonov@mail.ru>
2024-03-22 14:20:03 +0300

    1.3. Арифметические операции

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -1,13 +1,14 @@
 package ru.calculator;
+
 public class Calculator {
-    public static void main(String[] args){
-    int six = 6;
-    int four = 4;
-    int five = 5;
-    int two = 2;
-    int sixDivTwo = six / two;
-    int fiveMinusTwo = five - two;
-    int fourTimeTwo = four * two;
+    public static void main(String[] args) {
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int two = 2;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
