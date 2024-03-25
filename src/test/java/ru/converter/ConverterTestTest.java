82b8282881c8fead2127e40b4aa5dbb597cdb28d
Aleksandr <a.v.agafonov@mail.ru>
2024-03-25 18:08:29 +0300

    2. Что такое import

--- src/test/java/ru/converter/ConverterTestTest.java
+++ src/test/java/ru/converter/ConverterTestTest.java
@@ -43,7 +43,7 @@ class ConverterTestTest {
 
     @Test
     void whenConvertNegativeRblToDollarThenZeroDollar() {
-        float in = -50;
+        float in = -70;
         float expected = 0;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0F;
