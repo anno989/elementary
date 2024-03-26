25880fe3aab3edc1f02e58858a6fe26785a1fa07
Aleksandr <a.v.agafonov@mail.ru>
2024-03-26 17:16:12 +0300

    2.5. Тесты для определения расстояния между точками

--- src/main/java/ru/condition/X2.java
+++ src/main/java/ru/condition/X2.java
@@ -10,7 +10,7 @@ public class X2 {
         int a = 1;
         int b = 1;
         int c = 1;
-        int x = 1;
+        int x = 2;
         int rsl = X2.calc(a, b, c, x);
         System.out.println(rsl);
     }
--- src/test/java/ru/condition/X2Test.java
+++ src/test/java/ru/condition/X2Test.java
@@ -6,46 +6,46 @@ import org.junit.jupiter.api.Test;
 class X2Test {
     @Test
     void whenA11B11C11X11Then3() {
-        int a1 = 1;
-        int b1 = 1;
-        int c1 = 1;
-        int x1 = 1;
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
         int expected = 3;
-        int rsl = X2.calc(a1, b1, c1, x1);
+        int rsl = X2.calc(a, b, c, x);
         assertThat(rsl).isEqualTo(expected);
     }
 
     @Test
     void whenA20B21C21X21Then2() {
-        int a2 = 0;
-        int b2 = 1;
-        int c2 = 1;
-        int x2 = 1;
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
         int expected = 2;
-        int rsl = X2.calc(a2, b2, c2, x2);
+        int rsl = X2.calc(a, b, c, x);
         assertThat(rsl).isEqualTo(expected);
 
     }
 
     @Test
     void whenA31B31C30X31Then2() {
-        int a3 = 1;
-        int b3 = 1;
-        int c3 = 0;
-        int x3 = 1;
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
         int expected = 2;
-        int rsl = X2.calc(a3, b3, c3, x3);
+        int rsl = X2.calc(a, b, c, x);
         assertThat(rsl).isEqualTo(expected);
     }
 
     @Test
     void whenA41B41C41X40Then1() {
-        int a4 = 1;
-        int b4 = 1;
-        int c4 = 1;
-        int x4 = 0;
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
         int expected = 1;
-        int rsl = X2.calc(a4, b4, c4, x4);
+        int rsl = X2.calc(a, b, c, x);
         assertThat(rsl).isEqualTo(expected);
     }
 }
