91ffc542a2bc9e936b8949a0cc9ec6a02a7dc104
Aleksandr <a.v.agafonov@mail.ru>
2024-03-26 17:22:45 +0300

    2.5. Тесты для определения расстояния между точками

--- src/test/java/ru/calculator/FitTest.java
+++ src/test/java/ru/calculator/FitTest.java
@@ -10,15 +10,15 @@ class FitTest {
     void whenMan187then100dot05() {
         short in = 187;
         double expected = 100;
-        double out = Math.round(Fit.manWeight(in));
-        assertThat(out).isEqualTo(expected);
+        double out = Fit.manWeight(in);
+        assertThat(out).isEqualTo(expected, withPrecision(0.1));
     }
 
     @Test
     void whenWoman168then66() {
         short in = 168;
         double expected = 67;
-        double out = Math.round(Fit.womanWeight(in));
-        assertThat(out).isEqualTo(expected);
+        double out = Fit.womanWeight(in);
+        assertThat(out).isEqualTo(expected, withPrecision(0.69)) ;
     }
 }
