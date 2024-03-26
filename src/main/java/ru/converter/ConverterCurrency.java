9fe8f72d6899488ac5c2c333bf95cf101330c620
Aleksandr <a.v.agafonov@mail.ru>
2024-03-26 17:43:37 +0300

    2.5. Модульные тесты

--- src/main/java/ru/converter/ConverterCurrency.java
+++ src/main/java/ru/converter/ConverterCurrency.java
@@ -1,6 +1,6 @@
 package ru.converter;
 
-public class Converter {
+public class ConverterCurrency {
     public static float rubleToEuro(float value) {
         if (value <= 0) {
             return 0;
@@ -18,11 +18,11 @@ public class Converter {
     public static void main(String[] args) {
         float in = 140;
         float expectedEUR = 1.4F;
-        float outEUR = Converter.rubleToEuro(in);
+        float outEUR = ConverterCurrency.rubleToEuro(in);
         boolean passedEUR = expectedEUR == outEUR;
         System.out.println("140 rubles are 1.4. Test result : " + passedEUR);
         float expectedUSD = 1.5555556F;
-        float outUSD = Converter.rubleToDollar(in);
+        float outUSD = ConverterCurrency.rubleToDollar(in);
         boolean passedUSD = expectedUSD == outUSD;
         System.out.println("140 rubles are 1.5555556. Test result : " + passedUSD);
     }
--- src/test/java/ru/converter/ConverterСurrencyTest.java
+++ src/test/java/ru/converter/ConverterСurrencyTest.java
@@ -9,7 +9,7 @@ class ConverterCurrencyTest {
     void whenConvert140RblThen1Dot4Euro() {
         float in = 140;
         float expected = 1.4f;
-        float out = Converter.rubleToEuro(in);
+        float out = ConverterCurrency.rubleToEuro(in);
         float eps = 0.0001f;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
@@ -18,7 +18,7 @@ class ConverterCurrencyTest {
     void whenConvert140RblThen1Dot5555Dlr() {
         float in = 140;
         float expected = 1.5555f;
-        float out = Converter.rubleToDollar(in);
+        float out = ConverterCurrency.rubleToDollar(in);
         float eps = 0.0001f;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
@@ -27,7 +27,7 @@ class ConverterCurrencyTest {
     void whenConvertZeroRblToEuroThenZeroEuro() {
         float in = 0;
         float eps = 0;
-        float expected = Converter.rubleToEuro(in);
+        float expected = ConverterCurrency.rubleToEuro(in);
         float out = 0;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
@@ -36,7 +36,7 @@ class ConverterCurrencyTest {
     void whenConvert100RblToDollarThen1Dot1111Dollar() {
         float in = 100;
         float expected = 1.1111F;
-        float out = Converter.rubleToDollar(in);
+        float out = ConverterCurrency.rubleToDollar(in);
         float eps = 0.0001f;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
@@ -45,7 +45,7 @@ class ConverterCurrencyTest {
     void whenConvertNegativeRblToDollarThenZeroDollar() {
         float in = -80;
         float expected = 0;
-        float out = Converter.rubleToDollar(in);
+        float out = ConverterCurrency.rubleToDollar(in);
         float eps = 0.0F;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
