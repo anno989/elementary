6c931a4edde176e28e386a39a9f0fef8d7731184
Aleksandr <a.v.agafonov@mail.ru>
2024-03-28 02:35:37 +0300

    3.1 Операторы ветвлений

---
+++ src/main/java/ru/condition/AlertDivByZero.java
@@ -0,0 +1,26 @@
+package ru.condition;
+
+public class AlertDivByZero {
+    public static void possibleDiv(int number) {
+        boolean result1 = number == 0;
+        if (result1) {
+            System.out.println("Could not div by 0.");
+        }
+        boolean result2 = number < 0;
+        if (result2) {
+            System.out.println("This is negative number.");
+        }
+    }
+
+    public static void main(String[] args) {
+        AlertDivByZero.possibleDiv(4);
+        AlertDivByZero.possibleDiv(0);
+        AlertDivByZero.possibleDiv(-4);
+    }
+}
+
+
+
+
+
+
