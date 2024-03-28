6a304446e82d83d43609eaac676f3a051833f487
Aleksandr <a.v.agafonov@mail.ru>
2024-03-28 11:55:59 +0300

    3.2 Оператор if c блоком else

--- src/main/java/ru/condition/AlertDivByZero.java
+++ src/main/java/ru/condition/AlertDivByZero.java
@@ -2,12 +2,10 @@ package ru.condition;
 
 public class AlertDivByZero {
     public static void possibleDiv(int number) {
-        boolean result1 = number == 0;
-        if (result1) {
+        if (number == 0) {
             System.out.println("Could not div by 0.");
         }
-        boolean result2 = number < 0;
-        if (result2) {
+        if (number < 0) {
             System.out.println("This is negative number.");
         }
     }
