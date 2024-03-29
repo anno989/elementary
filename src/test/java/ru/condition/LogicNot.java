5a6c7764cd0b021b26bdf54f295e54fe6f6a4cfc
Aleksandr <a.v.agafonov@mail.ru>
2024-03-29 21:39:20 +0300

    3.11 Логическое отрицание

--- src/test/java/ru/condition/LogicNot.java
+++ src/test/java/ru/condition/LogicNot.java
@@ -4,6 +4,7 @@ public class LogicNot {
         public static boolean isEven(int num) {
             return num % 2 == 0;
         }
+
         public static boolean isPositive(int num) {
             return num > 0;
         }
@@ -19,6 +20,19 @@ public class LogicNot {
         public static boolean evenOrNotPositive(int num) {
             return (num % 2 == 0) || !(num < 0);
         }
+
+        public static void main(String[] args) {
+        int number = 5;
+
+        System.out.println("Is " + number + " even? " + LogicNot.isEven(number));
+        System.out.println("Is " + number + " positive? " + LogicNot.isPositive(number));
+        System.out.println("Is " + number + " not even? " + LogicNot.notEven(number));
+        System.out.println("Is " + number + " not positive? " + LogicNot.notPositive(number));
+        System.out.println("Is " + number + " not even and positive? " + LogicNot.notEvenAndPositive(number));
+        System.out.println("Is " + number + " even or not positive? " + LogicNot.evenOrNotPositive(number));
+    }
 }
+
+
 
 
