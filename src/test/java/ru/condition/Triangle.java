be07d6b988497e1f04d73d0e3559c446f3c599ad
Aleksandr <a.v.agafonov@mail.ru>
2024-03-30 01:16:40 +0300

    3.13 Множественное логическое выражение И

--- src/test/java/ru/condition/Triangle.java
+++ src/test/java/ru/condition/Triangle.java
@@ -6,7 +6,7 @@ public class Triangle {
         return ab + ac > bc && ab + bc > ac && bc + ac > ab;
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println(exist(2, 2, 3));
     }
 }
