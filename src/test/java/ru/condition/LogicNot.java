530d617fbfa4bf4c29699bcd7ac38827d1b1ebcb
Aleksandr <a.v.agafonov@mail.ru>
2024-03-29 21:24:03 +0300

    3.11 Логическое отрицание

--- src/test/java/ru/condition/LogicNot.java
+++ src/test/java/ru/condition/LogicNot.java
@@ -3,7 +3,6 @@ package ru.condition;
 public class LogicNot {
         public static boolean isEven(int num) {
             return num % 2 == 0;
-
         }
 
         public static boolean isPositive(int num) {
