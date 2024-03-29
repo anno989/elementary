2f423678d0c26030d2034d8e0c89a73b12d050fb
Aleksandr <a.v.agafonov@mail.ru>
2024-03-29 23:20:05 +0300

    3.11 Логическое отрицание

--- src/test/java/ru/condition/LogicNot.java
+++ src/test/java/ru/condition/LogicNot.java
@@ -1,37 +1,37 @@
 package ru.condition;
 
 public class LogicNot {
-        public static boolean isEven(int num) {
-            return num % 2 == 0;
-        }
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
 
-        public static boolean isPositive(int num) {
-            return num > 0;
-        }
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
 
-        public static boolean notEven(int num) {
-            return !(num % 2 == 0);
-        }
+    public static boolean notEven(int num) {
+        return !isEven(num);
+    }
 
-        public static boolean notPositive(int num) {
-            return !(num < 0);
-        }
+    public static boolean notPositive(int num) {
+        return !isPositive(num);
+    }
 
-        public static boolean notEvenAndPositive(int num) {
-            return !(num % 2 == 0) && num > 0;
-        }
+    public static boolean notEvenAndPositive(int num) {
+        return notEven(num) && isPositive(num);
+    }
 
-        public static boolean evenOrNotPositive(int num) {
-            return (num % 2 == 0) || !(num < 0);
-        }
+    public static boolean evenOrNotPositive(int num) {
+        return isEven(num) || notPositive(num);
+    }
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         int number = 5;
-        System.out.println("Is " + number + " even? " + LogicNot.isEven(number));
-        System.out.println("Is " + number + " positive? " + LogicNot.isPositive(number));
-        System.out.println("Is " + number + " not even? " + LogicNot.notEven(number));
-        System.out.println("Is " + number + " not positive? " + LogicNot.notPositive(number));
-        System.out.println("Is " + number + " not even and positive? " + LogicNot.notEvenAndPositive(number));
-        System.out.println("Is " + number + " even or not positive? " + LogicNot.evenOrNotPositive(number));
+        System.out.println("Is " + number + " even? " + isEven(number));
+        System.out.println("Is " + number + " positive? " + isPositive(number));
+        System.out.println("Is " + number + " not even? " + notEven(number));
+        System.out.println("Is " + number + " not positive? " + notPositive(number));
+        System.out.println("Is " + number + " not even and positive? " + notEvenAndPositive(number));
+        System.out.println("Is " + number + " even or not positive? " + evenOrNotPositive(number));
     }
 }
